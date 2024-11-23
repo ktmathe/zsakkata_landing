368837f4b901edf200872e5ba16cc9b819e38947
ktmathe <ktmathe@gmail.com>
2024-11-23 13:12:10 +0100

    fixes design

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import preact from "@astrojs/preact";
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://zsakkata.netlify.app/",
-  integrations: [preact()],
+  integrations: [preact(), mdx()],
   output: 'server',
   adapter: netlify()
 });
