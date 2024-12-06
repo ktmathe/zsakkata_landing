2b7fcaa964487cea0f1d3bd1f0f223b0fec8a3b1
ktmathe <ktmathe@gmail.com>
2024-12-06 14:17:30 +0100

    refact: enhance navigation links with classes and improve menu toggle functionality

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,3 +2,17 @@ document.querySelector('.hamburger').addEventListener('click', ()=> {
     document.querySelector('.nav-links').classList.toggle('expanded');
 })
 
+document.querySelectorAll('.nav-link').forEach(item => {
+    item.addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.remove('expanded');
+    });
+});
+
+document.addEventListener('click', (event) => {
+    const navLinks = document.querySelector('.nav-links');
+    const isClickInside = navLinks.contains(event.target) || document.querySelector('.hamburger').contains(event.target);
+
+    if (!isClickInside) {
+        navLinks.classList.remove('expanded');
+    }
+});
